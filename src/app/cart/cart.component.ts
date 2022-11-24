b2a88b112719b92a079fc1fb9f29fd6197b29aed
Adrian Burgos <adrian.burgos.colas@gmail.com>
2022-11-24 01:57:27 -0500

    completed angular getting started tutorial

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent {
+  items = this.cartService.getItems();
+
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  onSubmit(): void {
+    this.cartService.clearCart();
+    window.alert(`
+Your order has been submitted
+Name: ${this.checkoutForm.value?.name}
+Address: ${this.checkoutForm.value?.address}
+${this.items.map((item:{name: string, price: number}) => item.name + " $" + item.price +"\n").join("")}
+`);
+    this.items = [];
+    this.checkoutForm.reset();
+  }
+
+}
